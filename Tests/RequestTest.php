bd21de3e477a30f039cd8f82d1e8751c7c678c81
Jack Tsang <jacktsang@yedpay.com>
2018-05-18 18:01:19 +0800

    test-request

---
+++ Tests/RequestTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Yedpay\Tests;
+
+use Yedpay\Curl\Request;
+use Yedpay\Curl\HttpRequest;
+
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    protected $path;
+    protected $token;
+    protected $parameters;
+    protected $method;
+
+    protected $request;
+
+    public function setUp()
+    {
+        $this->path = null;
+        $this->token = 'abcdefg';
+        $this->parameters = [
+                'gateway_id' => 1,
+                'currency' => 'HKD',
+                'amount' => 0.1,
+                'wallet' => 'CN',
+            ];
+        $this->method = 'POST';
+
+        $this->request = new Request();
+    }
+
+    public function tearDown()
+    {
+        $this->path = null;
+        $this->token = null;
+        $this->parameters = null;
+        $this->method = null;
+
+        $this->request = null;
+    }
+
+    public function test_exist_class()
+    {
+        $this->assertTrue($this->request instanceof HttpRequest);
+    }
+
+    public function test_execute()
+    {
+        $this->request->setOptionArray($this->path, $this->method, $this->parameters, $this->token);
+        $actual = $this->request->execute();
+        $this->assertEquals(null, $actual);
+    }
+
+    public function test_error()
+    {
+        $actual = $this->request->error();
+        $this->assertEquals(null, $actual);
+    }
+
+    public function test_close()
+    {
+        $actual = $this->request->close();
+        $this->assertEquals(null, $actual);
+    }
+}
--- src/Curl.php
+++ src/Curl.php
@@ -7,6 +7,49 @@ use Exception;
 
 class Curl
 {
+    protected $library;
+    protected $request;
+
+    /**
+     * Get the value of library
+     */
+    public function getLibrary()
+    {
+        return $this->library;
+    }
+
+    /**
+     * Set the value of library
+     *
+     * @return  self
+     */
+    public function setLibrary(Library $library)
+    {
+        $this->library = $library;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Set the value of request
+     *
+     * @return  self
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+
+        return $this;
+    }
+
     /**
      * call Yedpay API using Curl
      *
@@ -17,12 +60,12 @@ class Curl
      */
     public function call($path, $method, $parameters)
     {
-        $library = new Library();
+        $library = $this->getLibrary();
         $url = $library->getEndpoint() . $path;
         $token = $library->getAccessToken();
 
         try {
-            $curl = new Request();
+            $curl = $this->getRequest();
             $curl->setOptionArray($url, $method, $parameters, $token);
             $result = $curl->execute();
             $err = $curl->error();
